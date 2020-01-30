81563c7c3bf8fe20ecdbb9d00bc36f0670b35679
Mark Doughty <mdoughty@lincoln.ac.uk>
2020-01-30 14:25:55 +0000

    First commit of C# project

---
+++ TicketMachine.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M
+{
+    //class TicketMachine models a simple ticket machine
+    //It prints a ticket of a single price if there is enough money
+    class TicketMachine
+    {
+        //fields and properties
+
+
+        //methods
+        //insertMoney - receive an amount of money from the customer
+        //parameter: int
+        //return: void
+        public void insertMoney(int amount)
+        {
+
+        }
+
+        //printTicket - simulate the output of a ticket
+        //parameter: none
+        //return: void
+        public void printTicket()
+        {
+
+        }
+
+        //status - returns the current total of money in this machine
+        //parameter: none
+        //return: void
+        public void status()
+        {
+
+        }
+
+
+    }
+}
