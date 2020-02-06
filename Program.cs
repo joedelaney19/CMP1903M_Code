6ba165251f752aef4252fb2ccc44810f5eca7449
technician <technician@lincoln.ac.uk>
2020-02-06 12:47:41 +0000

    Implemented code from Workshop

--- Program.cs
+++ Program.cs
@@ -11,6 +11,45 @@ namespace CMP1903M
         static void Main(string[] args)
         {
             TicketMachine t = new TicketMachine();
+            t.ticketPrice = 10;
+            string choice = "y";
+            while(choice == "y")
+            {
+                Console.WriteLine("Insert money: ");
+                string money_input = Console.ReadLine();
+                int money_input_int = int.Parse(money_input);
+                t.insertMoney(money_input_int);
+
+                Console.WriteLine("Enter staff promo code: ");
+                string promo = Console.ReadLine();
+                bool promo_valid = false;
+                if(promo == t.promo)
+                {
+                    promo_valid = true;
+                }
+
+                Console.WriteLine("Enter 'print' to print ticket or 'refund' to refund deposit: ");
+                string print_input = Console.ReadLine().ToLower();
+                if(print_input == "print")
+                {
+                    t.printTicket(promo_valid);
+                }
+                if(print_input == "refund")
+                {
+                    t.refundMoney();
+                }
+
+                choice = "";
+                while (!(choice == "y" || choice == "n"))
+                {
+                    Console.WriteLine("Would you like to print another ticket? (Y/N): ");
+                    choice = Console.ReadLine().ToLower();
+                    if(!(choice == "y" || choice == "n"))
+                    {
+                        Console.WriteLine("Invalid choice");
+                    }
+                }
+            }
         }
     }
 }
--- TicketMachine.cs
+++ TicketMachine.cs
@@ -11,7 +11,44 @@ namespace CMP1903M
     class TicketMachine
     {
         //fields and properties
+        private string _promo = "SoCS2020";
 
+        private int _prevDeposit;
+        public int prevDeposit
+        {
+            get { return _prevDeposit; }
+        }
+        public string promo
+        {
+            get { return _promo; }
+        }
+
+        private float _ticketPrice;
+        public float ticketPrice
+        {
+            get { return _ticketPrice; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _ticketPrice = value;
+                }
+                else
+                {
+                    Console.WriteLine("Ticket price cannot be less than 0");
+                }
+            }
+        }
+        private float _balance;
+        public float balance
+        {
+            get { return _balance; }
+        }
+        private float _total;
+        public float total
+        {
+            get { return _total; }
+        }
 
         //methods
         //insertMoney - receive an amount of money from the customer
@@ -19,15 +56,50 @@ namespace CMP1903M
         //return: void
         public void insertMoney(int amount)
         {
+            if(amount > 0)
+            {
+                _prevDeposit = amount;
+                _balance = _balance + amount;
+            }
+            else
+            {
+                Console.WriteLine("Amount must be more than 0");
+            }
 
+        }
+
+        //refundMoney - refunds the amount of the previous deposit without printing ticket
+        //parameter: none
+        //return: void
+        public void refundMoney()
+        {
+            _balance = _balance - _prevDeposit;
+            Console.WriteLine($"Previous deposit ({_prevDeposit}) has been refunded");
         }
 
         //printTicket - simulate the output of a ticket
         //parameter: none
         //return: void
-        public void printTicket()
+        public void printTicket(bool discount)
         {
-
+            float ticketPrice_loc = _ticketPrice;
+            if(discount)
+            {
+                ticketPrice_loc = (float)(_ticketPrice * 0.8);
+            }
+            if (balance >= ticketPrice_loc)
+            {
+                _balance = _balance - ticketPrice_loc;
+                _total = _total + ticketPrice_loc;
+                Console.WriteLine("*****************");
+                Console.WriteLine("SoCS Line Ticket");
+                Console.WriteLine($"Price = £{ticketPrice_loc}");
+                Console.WriteLine("*****************");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient funds to print ticket");
+            }
         }
 
         //status - returns the current total of money in this machine
@@ -35,9 +107,18 @@ namespace CMP1903M
         //return: void
         public void status()
         {
-
+            Console.WriteLine($"Balance: {_balance}");
+            Console.WriteLine($"Total money in machine: {_total}");
+            Console.WriteLine($"Current ticket price: {_ticketPrice}");
         }
 
-
+        //empty - empties the contents of the ticket machine
+        //parameter: none
+        //return: void
+        public void empty()
+        {
+            Console.WriteLine($"Emptying machine - total emptied: {_total}");
+            _total = 0;
+        }
     }
 }
